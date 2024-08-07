f7ba010bf6a1a385af014cffb29c52f7f576ee48
Nika Kantina <nikakantina@gmail.com>
2024-08-06 23:42:56 -0700

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const nAbs = Math.abs(n);
+    const nArray = String(nAbs).split('');
+    const reversedArray =  nArray.reverse();
+    return Number(reversedArray.join(''));
 }
+
